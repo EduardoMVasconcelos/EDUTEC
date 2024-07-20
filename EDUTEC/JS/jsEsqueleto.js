e05291a4758020ee13afad759f7427a9e8311d79
EduardoMVasconcelos <142501013+EduardoMVasconcelos@users.noreply.github.com>
2024-07-19 22:17:09 -0300

    concerto do slider

---
+++ EDUTEC/JS/jsEsqueleto.js
@@ -0,0 +1,7 @@
+function toggle() {
+    let blur = document.getElementById('blur')
+    blur.classList.toggle('active')
+
+    let popup = document.getElementById('img-popup')
+    popup.classList.toggle('active')
+}
---
+++ EDUTEC/PG-INICIAL/script.js
@@ -0,0 +1,3 @@
+function alert() {
+    window.alert("Seu Feedback foi enviado e em breve será encarregado aos nossos desenvolvedores. Agradecemos pelo seu Feedback!")
+}
