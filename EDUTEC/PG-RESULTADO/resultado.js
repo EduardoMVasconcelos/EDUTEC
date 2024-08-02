a4201824c66eb6a7c9669b6a0a10b53adfc441de
EduardoMVasconcelos <142501013+EduardoMVasconcelos@users.noreply.github.com>
2024-08-01 21:03:18 -0300

    finaliação da pagina resultados

---
+++ EDUTEC/PG-RESULTADO/resultado.js
@@ -0,0 +1,11 @@
+let resultado = document.querySelector("#resultado")
+let numeroResultado = 7
+resultado.innerHTML = `Você acertou ${numeroResultado}/12 de questões`
+
+document.querySelector("#reiniciar").onclick = () => {
+    location.href = "../PG-PERGUNTAS/perguntas.html"
+}
+
+document.querySelector("#inicio").onclick = () => {
+    location.href = "../PG-INICIAL/index.html"
+}
