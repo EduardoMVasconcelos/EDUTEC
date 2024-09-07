01a7671fe74baf32411a3e5fdb581511f3a60388
EduardoMVasconcelos <142501013+EduardoMVasconcelos@users.noreply.github.com>
2024-09-07 17:23:58 -0300

    adição do js

---
+++ script.js
@@ -0,0 +1,57 @@
+let slider = document.querySelector(".slider-container")
+let innerSlider = document.querySelector(".slider-inner")
+
+let pressed = false
+let startx
+let x
+
+slider.addEventListener('mousedown', (e)=> {
+    pressed = true
+    startx = e.offsetX - innerSlider.offsetLeft
+    slider.style.cursor = 'grabbing'
+})
+
+slider.addEventListener('mouseenter', ()=>{
+    slider.style.cursor = 'grab'
+})
+
+slider.addEventListener('mouseleave', ()=>{
+    slider.style.cursor = 'default'
+})
+
+slider.addEventListener('mouseup', ()=>{
+    slider.style.cursor = 'grab'
+})
+
+window.addEventListener('mouseup', ()=>{
+    pressed = false
+})
+
+slider.addEventListener('mousemove', (e)=> {
+    if(!pressed) return
+    e.preventDefault()
+
+    x = e.offsetX
+
+    innerSlider.style.left = `${x - startx}px`
+
+    checkboundary()
+})
+
+function checkboundary() {
+    let outer = slider.getBoundingClientRect()
+    let inner = innerSlider.getBoundingClientRect()
+
+    if(parseInt(innerSlider.style.left) > 0) {
+        innerSlider.style.left = '0px'
+    }else if(inner.right < outer.right) {
+        innerSlider.style.left = `-${inner.width - outer.width}px`
+    }
+}
+
+checkboundary()
+
+// Feedback
+function togglePopup() {
+    document.getElementById("popup-1").classList.toggle("active")
+}
