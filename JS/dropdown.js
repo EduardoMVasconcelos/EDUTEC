ce9839e037a3b630bd6958c749a7330e1ff0cc95
EduardoMVasconcelos <eduardo.m.vasconcelos@eaportal.org>
2024-11-21 20:25:33 -0300

    primeiro commit pag ranking

--- JS/dropdown.js
+++ JS/dropdown.js
@@ -4,3 +4,8 @@ const menu = document.querySelector(".menu")
 dropdown.onclick = () => {
     menu.classList.toggle("ativo")
 }
+
+function login() {
+    const login = document.querySelector(".login")
+    login.classList.add(ativado)
+}
